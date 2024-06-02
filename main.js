404be26f00a77493cc58fcad0dcd72563ad87d14
Ahmet İstemihan ÖZTÜRK <declarck@gmail.com>
2024-06-02 16:07:41 +0300

    L6-end-personal

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    }
-})
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "Green",
+          image: "./assets/images/socks_green.jpg",
+          bgColor: "#29c079",
+        },
+        {
+          id: 2235,
+          color: "Blue",
+          image: "./assets/images/socks_blue.jpg",
+          bgColor: "#3a4e67",
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    decrement() {
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
