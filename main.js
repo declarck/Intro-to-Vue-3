3a99f42ffa58fd34a786a273532982954c157856
Ahmet İstemihan ÖZTÜRK <declarck@gmail.com>
2024-06-02 12:53:06 +0300

    Test

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      imag2: "./assets/images/socks_blue.jpg",
+      // solution
+      url: "https://www.vuemastery.com/",
+      url2: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
+      // solution
+    };
+  },
+});
