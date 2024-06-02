d23c9c2a1e5d51fbbecd7dee010773491a8b4d4a
Ahmet İstemihan Öztürk <declarck@gmail.com>
2024-06-03 00:30:26 +0300

    L9-end-personal

---
+++ components/ProductDetails.js
@@ -0,0 +1,21 @@
+app.component('product-details', {
+    props: {
+
+    },
+    template:
+        /*html*/
+        `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>`,
+    data() {
+        return {
+            details: ['50% cotton', '30% wool', '20% polyester'],
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+})
